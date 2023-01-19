9a9bf634a89d5ef7cc96967057faaeaab4182249
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-01-19 15:24:18 +0100

    Styling for the game.html

---
+++ game.js
@@ -0,0 +1,7 @@
+console.log('Hello world!');
+
+const question = document.querySelector('#question');
+let choices = document.querySelectorAll('.choice-text');
+
+console.log(question);
+console.log(choices);
